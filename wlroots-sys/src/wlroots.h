609b8cc799dbe88d056fa6967a24749802d55efc
Timidger <APragmaticPlace@gmail.com>
2017-06-04 13:59:47 -0500

    Fixed wlroots-sys build

--- wlroots-sys/src/wlroots.h
+++ wlroots-sys/src/wlroots.h
@@ -1,3 +1,9 @@
-#include <wlr/wayland.h>
+#include <wlr/types.h>
+#include <wlr/session.h>
 #include <wlr/backend.h>
-#include <wlr/backend/wayland.h>
+// TODO Does not build properly
+//#include <wlr/backend/wayland.h>
+#include <wlr/backend/drm.h>
+#include <wlr/backend/udev.h>
+#include <wlr/common/list.h>
+#include <wlr/common/log.h>
