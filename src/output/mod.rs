0131de7e0201f70a4075f213da9daca1678ce5ce
Timidger <APragmaticPlace@gmail.com>
2017-06-15 18:04:43 +1000

    removed WeakOutput, for now

--- src/output/mod.rs
+++ src/output/mod.rs
@@ -1,10 +1,8 @@
 //! Methods and structs that control the physical outputs and what they display.
 
 
-mod weak_output;
 mod output;
 
-pub use self::weak_output::WeakOutput;
 pub use self::output::Output;
 
 use std::sync::Mutex;
