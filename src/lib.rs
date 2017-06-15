cd7884af57b48b02ac351b2d0576f0568157c967
Timidger <APragmaticPlace@gmail.com>
2017-06-15 18:56:08 +1000

    Removed macros.rs, added utils.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,11 @@ pub extern crate wlroots_sys;
 #[macro_use] extern crate wayland_sys;
 #[macro_use] extern crate error_chain;
 
-pub mod macros;
 mod output;
 mod session;
 mod backend;
+pub mod utils;
 
 pub use session::{Session, SessionErr};
 pub use backend::Backend;
+pub use utils::*;
