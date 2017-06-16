f134817ed9cd0154ca6892995477b5acacc43cde
Timidger <APragmaticPlace@gmail.com>
2017-06-16 20:32:03 +1000

    Export those macros (wl_container_of mainly)

--- src/utils.rs
+++ src/utils.rs
@@ -5,6 +5,7 @@ use std::ptr;
 use wlroots_sys::{wl_list, wl_signal};
 use wayland_sys::server::{WAYLAND_SERVER_HANDLE, wl_notify_func_t};
 
+#[macro_export]
 macro_rules! offset_of {
     ($ty:ty, $field:ident) => {
         &(*(0 as *const $ty)).$field as *const _ as usize
@@ -12,6 +13,7 @@ macro_rules! offset_of {
 }
 
 
+#[macro_export]
 macro_rules! wl_container_of {
     ($ptr:ident, $ty:ty, $field:ident) => {
         ($ptr as usize - offset_of!($ty, $field)) as *const $ty
