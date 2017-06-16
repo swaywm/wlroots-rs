938094384d833b18342aeabec7c323bdb824101f
Timidger <APragmaticPlace@gmail.com>
2017-06-16 17:43:45 +1000

    Updated example to use 90% safe code so far

--- examples/simple.rs
+++ examples/simple.rs
@@ -6,7 +6,8 @@ use std::ptr::null_mut;
 use std::env;
 use std::time::Instant;
 use std::os::raw::{c_void, c_int};
-
+use std::cell::RefCell;
+use std::sync::atomic::{AtomicBool, Ordering};
 
 struct State {
     /// The color on the screen.
@@ -32,28 +33,31 @@ fn main() {
     } else if env::var("WAYLAND_DISPLAY").is_ok() {
         panic!("Detected that Wayland is running. Run this in its own virtual terminal")
     }
-    let mut session = wlroots::Session::new()
-        .expect("Could not start session");
-    unsafe {
-        // init output (this will eventually be safe).
-        wlroots::output::init(&mut session);
-    }
-    // set loop to break after 3 seconds.
-    let mut done = false;
-    // TODO This is unsafe, need to make this not pass and require a RefCell
-    session.set_timeout(&mut done,
-                        |is_done: &mut bool| *is_done = true,
-                        3000);
-    session.backend.init().expect("Backend could not initalize");
-    while done {
-        match session.dispatch_event_loop() {
-            0 => {}
-            err_code => {
-                println!("Error: {:?}", err_code);
-                break;
+    let mut done = AtomicBool::new(false);
+    {
+        let mut session = wlroots::Session::new()
+            .expect("Could not start session");
+        unsafe {
+            // init output (this will eventually be safe).
+            wlroots::output::init(&mut session);
+        }
+        // set loop to break after 3 seconds.
+        // TODO This is unsafe, need to make this not pass and require a RefCell
+        session.set_timeout(&done,
+                            |done: &AtomicBool|
+                            done.store(true, Ordering::Relaxed),
+                            3000);
+        session.backend.init().expect("Backend could not initalize");
+        while done.load(Ordering::Relaxed) {
+            match session.dispatch_event_loop() {
+                0 => {}
+                err_code => {
+                    println!("Error: {:?}", err_code);
+                    break;
+                }
             }
         }
+        // TODO Ensure that this all cleaned up properly in RAII
     }
-    // TODO Ensure that this all cleaned up properly in RAII
 
 }
