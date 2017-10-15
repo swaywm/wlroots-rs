ab270d8591aabb34984e0bc0b83838791777b1f4
Timidger <APragmaticPlace@gmail.com>
2017-10-14 19:06:53 -0700

    Added feature flag for the example

--- wlroots-sys/build.rs
+++ wlroots-sys/build.rs
@@ -59,46 +59,46 @@ fn main() {
 
     meson();
 
-    // Example Khronos building stuff
-    // TODO Put behind feature flag?
-    let dest = env::var("OUT_DIR").unwrap();
-    let mut file = File::create(&Path::new(&dest).join("bindings.rs")).unwrap();
-    Registry::new(Api::Gl, (4, 5), Profile::Core, Fallbacks::All, [])
-        .write_bindings(StaticGenerator, &mut file)
-        .unwrap();
+    if cfg!(feature = "example") {
 
-    // Build share.d for examples
-    // TODO put behind feature flag to reduce build times
-    let example_generated = bindgen::builder()
-        .derive_debug(true)
-        .derive_default(true)
-        .generate_comments(true)
-        .header("wlroots/examples/shared.h")
-        .clang_arg("-Iwlroots/include")
-        .clang_arg("-Iwlroots/include/wlr")
-        .clang_arg("-Iwlroots/include/xcursor")
-        .clang_arg("-I/usr/include/pixman-1")
-        // Work around bug https://github.com/rust-lang-nursery/rust-bindgen/issues/687
-        .hide_type("FP_NAN")
-        .hide_type("FP_INFINITE")
-        .hide_type("FP_ZERO")
-        .hide_type("FP_SUBNORMAL")
-        .hide_type("FP_NORMAL")
-        .generate().unwrap();
-    example_generated.write_to_file(format!("{}/shared.rs", dest)).unwrap();
+        // Example Khronos building stuff
+        let dest = env::var("OUT_DIR").unwrap();
+        let mut file = File::create(&Path::new(&dest).join("bindings.rs")).unwrap();
+        Registry::new(Api::Gl, (4, 5), Profile::Core, Fallbacks::All, [])
+            .write_bindings(StaticGenerator, &mut file)
+            .unwrap();
 
-    // Build shared.c for examples
-    // TODO Put behind feature flag
-    let mut config = gcc::Build::new();
-    config.flag("-Wall");
-    config.flag("-Wpedantic");
-    config.flag("-Iwlroots/include");
-    config.flag("-Iwlroots/include/wlr");
-    config.file("wlroots/examples/shared.c");
-    config.compile("libshared.a");
+        // Build share.d for examples
+        let example_generated = bindgen::builder()
+            .derive_debug(true)
+            .derive_default(true)
+            .generate_comments(true)
+            .header("wlroots/examples/shared.h")
+            .clang_arg("-Iwlroots/include")
+            .clang_arg("-Iwlroots/include/wlr")
+            .clang_arg("-Iwlroots/include/xcursor")
+            .clang_arg("-I/usr/include/pixman-1")
+            // Work around bug https://github.com/rust-lang-nursery/rust-bindgen/issues/687
+            .hide_type("FP_NAN")
+            .hide_type("FP_INFINITE")
+            .hide_type("FP_ZERO")
+            .hide_type("FP_SUBNORMAL")
+            .hide_type("FP_NORMAL")
+            .generate().unwrap();
+        example_generated.write_to_file(format!("{}/shared.rs", dest)).unwrap();
 
-    // Link against libpam
-    println!("cargo:rustc-flags=-l shared")
+        // Build shared.c for examples
+        let mut config = gcc::Build::new();
+        config.flag("-Wall");
+        config.flag("-Wpedantic");
+        config.flag("-Iwlroots/include");
+        config.flag("-Iwlroots/include/wlr");
+        config.file("wlroots/examples/shared.c");
+        config.compile("libshared.a");
+
+        // Link against libpam
+        println!("cargo:rustc-flags=-l shared")
+    }
 }
 
 #[cfg(not(feature = "static"))]
