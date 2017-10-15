3ee26defc551b75c53fced5feca4d2c067c3f6e5
Timidger <APragmaticPlace@gmail.com>
2017-10-14 19:31:48 -0700

    Cleaned house in wlroots-rs
    It was all old crap anyways

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,4 @@
 pub extern crate wlroots_sys;
 #[macro_use] extern crate lazy_static;
 #[macro_use] extern crate wayland_sys;
-#[macro_use] extern crate error_chain;
 
-pub mod output;
-mod session;
-mod backend;
-pub mod utils;
-
-pub use session::{Session, SessionErr};
-pub use backend::Backend;
-pub use utils::*;
