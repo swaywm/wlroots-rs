ecb37b012e59c7d3eabc11308b301b691ae5fdc7
Timidger <APragmaticPlace@gmail.com>
2017-11-08 18:04:22 -0800

    Renamed manager/keyboard module to keyboard_handler

--- src/manager/mod.rs
+++ src/manager/mod.rs
@@ -1,11 +1,11 @@
 mod input_manager;
 mod output_manager;
-mod keyboard;
+mod keyboard_handler;
 mod pointer;
 mod output;
 
 pub use self::input_manager::{InputManager, InputManagerHandler};
-pub use self::keyboard::{KeyboardHandler, KeyboardWrapper};
+pub use self::keyboard_handler::{KeyboardHandler, KeyboardWrapper};
 pub use self::output::{Output, OutputHandler};
 pub use self::output_manager::{OutputManager, OutputManagerHandler};
 pub use self::pointer::{Pointer, PointerHandler};
