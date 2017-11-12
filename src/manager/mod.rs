685ffc24d106398b740f42898f716c83f19f70f3
Timidger <APragmaticPlace@gmail.com>
2017-11-11 20:33:23 -0800

    mod user_output -> output_handler

--- src/manager/mod.rs
+++ src/manager/mod.rs
@@ -2,11 +2,11 @@ mod input_manager;
 mod output_manager;
 mod keyboard_handler;
 mod pointer_handler;
-mod user_output;
+mod output_handler;
 
 pub use self::input_manager::{InputManager, InputManagerHandler};
 pub use self::keyboard_handler::{KeyboardHandler, KeyboardWrapper};
+pub use self::output_handler::{OutputHandler, UserOutput};
 pub use self::output_manager::{OutputBuilder, OutputBuilderResult, OutputManager,
                                OutputManagerHandler};
 pub use self::pointer_handler::{PointerHandler, PointerWrapper};
-pub use self::user_output::{OutputHandler, UserOutput};
