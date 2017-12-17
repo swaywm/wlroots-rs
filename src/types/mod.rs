918b4c4b6aecf4a3e4cda6a1aff4d570cd877cb9
Timidger <APragmaticPlace@gmail.com>
2017-12-17 12:56:33 -0800

    Added Area type (wrapper for wlr_box)

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -3,7 +3,9 @@ pub mod pointer;
 pub mod input_device;
 pub mod keyboard;
 pub mod output;
+pub mod area;
 
+pub use self::area::*;
 pub use self::cursor::*;
 pub use self::input_device::*;
 pub use self::keyboard::*;
