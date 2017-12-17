10d79f15daec6a5063f70f4b005915b46d9b10eb
Timidger <APragmaticPlace@gmail.com>
2017-12-17 13:34:02 -0800

    Filled out the rest of the ex config structs

--- example-config/src/lib.rs
+++ example-config/src/lib.rs
@@ -5,27 +5,37 @@ extern crate ini;
 extern crate wlroots;
 extern crate wlroots_sys;
 
+use std::path::PathBuf;
+
 use wlroots_sys::wl_output_transform;
 
-//use wlroots::Area;
+use wlroots::types::Area;
 
+/// Main example configuration, holds the configuration sections for
+/// outputs and devices.
+pub struct ExampleConfig {
+    pub config_path: PathBuf,
+    pub mapped_output: String,
+    pub mapped_box: Area,
+    pub outputs: Vec<OutputConfig>,
+    pub devs: Vec<DevConfig>
+}
+
+/// Configuration section for outputs.
 pub struct OutputConfig {
-    name: String,
-    transform: wl_output_transform,
-    x: i32,
-    y: i32
+    pub name: String,
+    pub transform: wl_output_transform,
+    pub x: i32,
+    pub y: i32
 }
 
+/// Configuration section for devices.
 pub struct DevConfig {
-    name: String,
-    mapped_output: String,
-    //mapped_box: Area
+    pub name: String,
+    pub mapped_output: String,
+    pub mapped_box: Area
 }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+pub fn parse_args(args: &[&str]) -> Option<ExampleConfig> {
+    unimplemented!()
 }
