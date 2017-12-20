ab2b4d7114cbf65922b5db1877a27a422e9fe9c9
Timidger <APragmaticPlace@gmail.com>
2017-12-20 17:18:05 -0600

    Fix doc tests

--- src/lib.rs
+++ src/lib.rs
@@ -12,12 +12,15 @@
 //! struct InputManager;
 //! struct OutputManager;
 //!
-//! impl wlroots::manager::OutputManagerHandler for OutputManager {}
-//! impl wlroots::manager::InputManagerHandler for InputManager {}
+//! impl wlroots::OutputManagerHandler for OutputManager {}
+//! impl wlroots::InputManagerHandler for InputManager {}
 //!
 //! fn main() {
-//!     wlroots::compositor::Compositor::new(Box::new(InputManager),
-//!     Box::new(OutputManager)).run()
+//!     wlroots::CompositorBuilder::new()
+//!          .build_auto((), // Dummy state
+//!                      Box::new(InputManager),
+//!                      Box::new(OutputManager))
+//!          .run()
 //! }
 //! ```
 
@@ -27,7 +30,7 @@ extern crate bitflags;
 extern crate lazy_static;
 extern crate libc;
 #[macro_use]
-extern crate wayland_sys;
+pub extern crate wayland_sys;
 pub extern crate wlroots_sys;
 pub extern crate xkbcommon;
 
