1ea3866ed37d5690ac02ccf943c9e2a32d21e321
Timidger <APragmaticPlace@gmail.com>
2018-01-04 14:39:31 -0600

    Added C string to Rust string util function

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,7 @@
 //! Utility functions for use within wlroots-rs
 
-use std::ffi::CString;
+use std::ffi::{CStr, CString};
+use std::os::raw::c_char;
 use std::process::exit;
 
 /// Converts a Rust string into C string without error handling.
@@ -17,5 +18,15 @@ pub fn safe_as_cstring<S>(string: S) -> CString
                      err);
             exit(1)
         }
+    }
+}
+
+/// Converts a C string into a Rust string without error handling.
+/// The pointer passed to this function _must_ be valid.
+pub unsafe fn c_to_rust_string(c_str: *const c_char) -> Option<String> {
+    if c_str.is_null() {
+        None
+    } else {
+        Some(CStr::from_ptr(c_str).to_string_lossy().into_owned())
     }
 }
