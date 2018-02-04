05e0672ba332c9ff854eceee09d4599843390a54
Timidger <APragmaticPlace@gmail.com>
2018-02-04 15:43:49 -0500

    fmt

--- src/types/seat/mod.rs
+++ src/types/seat/mod.rs
@@ -3,7 +3,7 @@ pub mod seat;
 pub mod grab;
 pub mod touch_point;
 
-pub use self::seat_client::*;
-pub use self::seat::*;
 pub use self::grab::*;
+pub use self::seat::*;
+pub use self::seat_client::*;
 pub use self::touch_point::*;
