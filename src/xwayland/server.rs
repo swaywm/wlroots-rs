b2328d984cdb500ce92c6d6f08af51b5e6a10a7e
Timidger <APragmaticPlace@gmail.com>
2018-04-28 20:05:46 -0400

    Added field accessors for XwaylandServer

--- src/xwayland/server.rs
+++ src/xwayland/server.rs
@@ -1,11 +1,13 @@
-use super::{XWaylandManager, XWaylandManagerHandler};
+use libc::c_int;
 use wayland_sys::server::signal::wl_signal_add;
 use wlroots_sys::{wl_display, wlr_compositor, wlr_xwayland, wlr_xwayland_create,
-                  wlr_xwayland_destroy};
+                  wlr_xwayland_destroy, pid_t, wl_client};
+
+use super::{XWaylandManager, XWaylandManagerHandler};
 
 pub struct XWaylandServer {
-    manager: Box<XWaylandManager>,
-    xwayland: *mut wlr_xwayland
+    xwayland: *mut wlr_xwayland,
+    manager: Box<XWaylandManager>
 }
 
 impl XWaylandServer {
@@ -23,6 +25,31 @@ impl XWaylandServer {
             panic!("Could not start XWayland server")
         }
         XWaylandServer { xwayland, manager }
+    }
+
+    /// Get the PID of the XWayland server.
+    pub fn pid(&self) -> pid_t {
+        unsafe { (*self.xwayland).pid }
+    }
+
+    pub fn display(&self) -> c_int {
+        unsafe { (*self.xwayland).display }
+    }
+
+    pub fn x_fd(&self) -> [c_int; 2] {
+        unsafe { (*self.xwayland).x_fd }
+    }
+
+    pub fn wl_fd(&self) -> [c_int; 2] {
+        unsafe { (*self.xwayland).wl_fd }
+    }
+
+    pub fn wm_fd(&self) -> [c_int; 2] {
+        unsafe {(*self.xwayland).wm_fd }
+    }
+
+    pub fn wl_client(&self) -> *mut wl_client {
+        unsafe { (*self.xwayland).client }
     }
 }
 
