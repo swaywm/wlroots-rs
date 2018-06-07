0686905916aace5b83cd296b971e313875551e03
Tony Crisci <tony@dubstepdish.com>
2018-06-07 19:17:44 -0400

    implement Hash and PartialEq for DragIconHandle

--- src/types/seat/drag_icon.rs
+++ src/types/seat/drag_icon.rs
@@ -1,6 +1,7 @@
 use wlroots_sys::wlr_drag_icon;
 use std::cell::Cell;
 use std::rc::{Rc, Weak};
+use std::hash::{Hash, Hasher};
 use std::panic;
 use errors::{HandleErr, HandleResult};
 use DragIconHandler;
@@ -74,9 +75,24 @@ impl DragIcon {
     }
 }
 
+#[derive(Debug, Clone)]
 pub struct DragIconHandle {
     handle: Weak<Cell<bool>>,
     drag_icon: *mut wlr_drag_icon
+}
+
+impl Eq for DragIconHandle {}
+
+impl PartialEq for DragIconHandle {
+    fn eq(&self, rhs: &Self) -> bool {
+        self.drag_icon == rhs.drag_icon
+    }
+}
+
+impl Hash for DragIconHandle {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.drag_icon.hash(state);
+    }
 }
 
 impl DragIconHandle {
