96c8f85fa9c18e2bc22fe2400ecc613e4d036fa7
Timidger <APragmaticPlace@gmail.com>
2018-11-26 00:47:17 -0500

    Expose dehandle crate
    For some reason the documentation doesn't generate when we re-export it,
    and you have to click through the crate manually.
    
    Whatever, this works.

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ extern crate vsprintf;
 #[macro_use]
 pub extern crate wayland_sys;
 pub extern crate wlroots_sys;
-extern crate wlroots_dehandle;
+pub extern crate wlroots_dehandle;
 pub extern crate xkbcommon;
 
 pub use wlroots_dehandle::wlroots_dehandle;
