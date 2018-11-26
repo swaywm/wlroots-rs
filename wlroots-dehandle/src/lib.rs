1128d2d15c39df6b3bf748b7359c7d1a6696f0c5
Timidger <APragmaticPlace@gmail.com>
2018-11-25 22:24:55 -0500

    Handle all syntax cases
    Was annoying, but now all the cases are covered.

--- wlroots-dehandle/src/lib.rs
+++ wlroots-dehandle/src/lib.rs
@@ -9,9 +9,10 @@ use std::collections::HashSet;
 
 use proc_macro::TokenStream;
 use proc_macro2::Ident;
-use syn::{ItemFn, Stmt, UseTree, ItemUse, Item,
+use syn::{ItemFn, Stmt, UseTree, ItemUse, Item, Block, Expr,
           parse::{self, Parse, ParseStream},
-          punctuated::Punctuated};
+          punctuated::Punctuated,
+          fold::Fold};
 
 /// Parses a list of variable names separated by commas
 ///
@@ -35,8 +36,13 @@ impl Parse for Args {
     }
 }
 
+impl Fold for Args {
+    fn fold_block(&mut self, block: Block) -> Block {
+        build_block(block.stmts.iter(), self)
+    }
+}
+
 impl Args {
-    // TODO Pop and then report error if not all used
     fn is_handle(&self, name: Ident) -> bool {
         self.vars.contains(&name)
     }
@@ -60,18 +66,18 @@ impl Args {
 /// impl InputManagerHandler for InputManager {
 ///     #[wlroots_dehandle(compositor, keyboard, seat)]
 ///     fn keyboard_added(&mut self,
-///                       compositor: CompositorHandle,
+///                       compositor_handle: CompositorHandle,
 ///                       keyboard: KeyboardHandle)
 ///                       -> Option<Box<Keyboard Handler>> {
-///         use compositor as compositor;
+///         use compositor_handle as compositor;
 ///         let keyboard = keyboard;
 ///         use compositor as compositor;
 ///         use keyboard as keyboard;
 ///         let server: &mut ::Server = compositor.into();
 ///         server.keyboards.push(keyboard.weak_reference());
 ///         // Now that we have at least one keyboard, update the seat capabilities.
-///         let seat = &server.seat.seat;
-///         use seat as seat;
+///         let server_seat = &server.seat.seat;
+///         use server_seat as seat;
 ///         let mut capabilities = seat.capabilities();
 ///         capabilities.insert(Capability::Keyboard);
 ///         seat.set_capabilities(capabilities);
@@ -82,39 +88,152 @@ impl Args {
 /// ```
 #[proc_macro_attribute]
 pub fn wlroots_dehandle(args: TokenStream, input: TokenStream) -> TokenStream {
-    let mut input = parse_macro_input!(input as ItemFn);
-    let args = parse_macro_input!(args as Args);
-    let output = build_block(input.block.stmts.iter(), &args);
-    input.block = parse_quote!({#(#output)*});
-    TokenStream::from(quote!(#input))
+    let input = parse_macro_input!(input as ItemFn);
+    let mut args = parse_macro_input!(args as Args);
+    let output = args.fold_item_fn(input);
+    TokenStream::from(quote!(#output))
 }
 
-fn build_block(mut input: std::slice::Iter<Stmt>, args: &Args) -> Vec<Stmt> {
+fn build_block(mut input: std::slice::Iter<Stmt>, args: &Args) -> Block {
     let mut output = vec![];
     let mut inner = None;
     while let Some(stmt) = input.next().cloned() {
-        use {Stmt::Item, Item::Use, UseTree::Rename};
+        use {Item::Use, UseTree::Rename};
         match stmt.clone() {
-            Item(Use(ItemUse { tree: Rename(use_stmt), ..})) => {
-                if args.is_handle(use_stmt.ident.clone()) {
+            Stmt::Item(Use(ItemUse { tree: Rename(use_stmt), ..})) => {
+                if args.is_handle(use_stmt.rename.clone()) {
                     inner = Some((use_stmt.ident, use_stmt.rename));
                     break
                 }
                 output.push(stmt)
             },
+            // Recurse into function body
+            Stmt::Item(Item::Fn(mut function)) => {
+                let inner_block = function.block.clone();
+                *function.block = build_block(inner_block.stmts.iter(), args);
+                output.push(Stmt::Item(Item::Fn(function)))
+            },
+            // Recurse into let call
+            Stmt::Local(mut local) => {
+                if let Some((_, body)) = local.init.clone() {
+                    let body = build_block_expr(*body.clone(), args);
+                    let body = parse_quote!(#body);
+                    local.init.as_mut().unwrap().1 = body;
+                    output.push(Stmt::Local(local))
+                }
+            },
+            Stmt::Expr(expr) => {
+                let body = build_block_expr(expr, args);
+                output.push(parse_quote!({#body}))
+            }
+            Stmt::Semi(expr, _) => {
+                let body = build_block_expr(expr, args);
+                output.push(parse_quote!({#body;}))
+            }
             _ => output.push(stmt)
         }
     }
     if let Some((handle, dehandle)) = inner {
-        let inner_output = build_block(input, args);
-        let inner_block = parse_quote!(
-            (#handle).run(|#dehandle|{
-                #(#inner_output)*
+        let inner_block = build_block(input, args);
+        let handle_call = parse_quote!(
+            {(#handle).run(|#dehandle|{
+                #inner_block
             }).expect(concat!("Could not upgrade handle ",
                               stringify!(#handle), " to ",
-                              stringify!(#dehandle)));
+                              stringify!(#dehandle)))}
         );
-        output.push(inner_block);
+        output.push(handle_call);
     }
-    output
+    parse_quote!({#(#output)*})
+}
+
+/// Tries to build a block from the expression.
+fn build_block_expr(expr: Expr, args: &Args) -> Expr {
+    match expr {
+        Expr::Block(block) => {
+            let block = build_block(block.block.stmts.iter(), args);
+            parse_quote!(#block)
+        }
+        Expr::Let(mut let_expr) => {
+            *let_expr.expr = build_block_expr(*let_expr.expr.clone(), args);
+            Expr::Let(let_expr)
+        },
+        Expr::If(mut if_expr) => {
+            let then_branch = if_expr.then_branch.clone();
+            let then_branch = build_block_expr(parse_quote!(#then_branch), args);
+            if_expr.then_branch = parse_quote!(#then_branch);
+            if_expr.else_branch = match if_expr.else_branch.clone() {
+                None => if_expr.else_branch,
+                Some((token, else_branch)) => {
+                    Some((token, Box::new( build_block_expr(*else_branch, args))))
+                }
+            };
+            Expr::If(if_expr)
+        },
+        Expr::While(mut while_expr) => {
+            let body = while_expr.body.clone();
+            let body = build_block_expr(parse_quote!(#body), args);
+            while_expr.body = parse_quote!(#body);
+            Expr::While(while_expr)
+        },
+        Expr::ForLoop(mut for_expr) => {
+            let body = for_expr.body.clone();
+            let body = build_block_expr(parse_quote!(#body), args);
+            for_expr.body = parse_quote!(#body);
+            Expr::ForLoop(for_expr)
+        },
+        Expr::Loop(mut loop_expr) => {
+            let body = loop_expr.body.clone();
+            let body = build_block_expr(parse_quote!(#body), args);
+            loop_expr.body = parse_quote!(#body);
+            Expr::Loop(loop_expr)
+        },
+        Expr::Match(mut match_expr) => {
+            for arm in &mut match_expr.arms {
+                *arm.body = build_block_expr(*arm.body.clone(), args)
+            }
+            Expr::Match(match_expr)
+        },
+        Expr::Closure(mut closure_expr) => {
+            *closure_expr.body = build_block_expr(*closure_expr.body.clone(),
+                                                  args);
+            Expr::Closure(closure_expr)
+        },
+        Expr::Unsafe(mut unsafe_expr) => {
+            unsafe_expr.block = build_block(unsafe_expr.block.stmts.iter(),
+                                            args);
+            Expr::Unsafe(unsafe_expr)
+        },
+        Expr::Assign(mut assign_expr) => {
+            *assign_expr.right = build_block_expr(*assign_expr.right.clone(),
+                                                  args);
+            Expr::Assign(assign_expr)
+        },
+        Expr::AssignOp(mut assign_expr) => {
+            *assign_expr.right = build_block_expr(*assign_expr.right.clone(),
+                                                  args);
+            Expr::AssignOp(assign_expr)
+        },
+        Expr::Break(mut break_expr) => {
+            match break_expr.expr {
+                None => {},
+                Some(ref mut expr) => {
+                    **expr = build_block_expr(*expr.clone(), args);
+                }
+            }
+            Expr::Break(break_expr)
+        },
+        Expr::Return(mut return_expr) => {
+            match return_expr.expr {
+                None => {},
+                Some(ref mut expr) => {
+                    **expr = build_block_expr(*expr.clone(), args);
+                }
+            }
+            Expr::Return(return_expr)
+        },
+        v => {
+            v
+        }
+    }
 }
