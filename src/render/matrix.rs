df5424f52b4a67535a8ac0f77ef6ffef78b431af
Timidger <APragmaticPlace@gmail.com>
2018-12-04 15:38:16 -0500

    Remove matrix name, move project_box to top

--- src/render/matrix.rs
+++ src/render/matrix.rs
@@ -18,70 +18,10 @@ pub const IDENTITY: [f32; 9] =[
     0.0, 0.0, 1.0
 ];
 
-/// Translate the matrix in x, and y.
-pub fn matrix_translate(x: f32, y: f32) -> [f32; 9] {
-    let mut output = [0.0; 9];
-    unsafe {
-        wlr_matrix_translate(output.as_mut_ptr(), x, y);
-    }
-    output
-}
-
-/// Scale the output in the x, and y.
-pub fn matrix_scale(x: f32, y: f32) -> [f32; 9] {
-    let mut output = [0.0; 9];
-    unsafe {
-        wlr_matrix_scale(output.as_mut_ptr(), x, y);
-    }
-    output
-}
-
-/// Rotate the matrix by some amount of radians.
-pub fn matrix_rotate(mut matrix: [f32; 9], radians: f32) -> [f32; 9] {
-    unsafe {
-        wlr_matrix_rotate(matrix.as_mut_ptr(), radians);
-    }
-    matrix
-}
-
-/// Multiply two matrices together.
-pub fn matrix_multiply(x: [f32; 9], y: [f32; 9]) -> [f32; 9] {
-    let mut output = [0.0; 9];
-    unsafe {
-        wlr_matrix_multiply(output.as_mut_ptr(), x.as_ptr(), y.as_ptr());
-    }
-    output
-}
-
-/// Transform the matrix based on the given Wayland output transform mode.
-pub fn matrix_transform(mut matrix: [f32; 9], transform: wl_output_transform) -> [f32; 9] {
-    unsafe {
-        wlr_matrix_transform(matrix.as_mut_ptr(), transform);
-    }
-    matrix
-}
-
-/// Transform the matrix based on the given Wayland output transform mode and
-/// the width and height of a texture.
-pub fn matrix_projection(mut matrix: [f32; 9],
-                         width: i32,
-                         height: i32,
-                         transform: wl_output_transform)
-                         -> [f32; 9] {
-    unsafe {
-        wlr_matrix_projection(matrix.as_mut_ptr(), width, height, transform);
-    }
-    matrix
-}
-
-pub fn matrix_transpose(matrix: [f32; 9]) -> [f32; 9] {
-    let mut result = [0.0; 9];
-    unsafe {
-        wlr_matrix_transpose(result.as_mut_ptr(), matrix.as_ptr());
-    }
-    result
-}
-
+/// Shortcut for the various matrix operations involved in projecting the
+/// specified wlr_box onto a given orthographic projection with a given
+/// rotation. The result can be applied to each coordinate of the box to
+/// get a new coordinate from [-1,1].
 pub fn project_box(area: Area,
                    transform: wl_output_transform,
                    rotation: f32,
@@ -96,4 +36,69 @@ pub fn project_box(area: Area,
                                projection.as_ptr());
         output
     }
+}
+
+/// Translate the 2D matrix to a magnitude of (x, y).
+pub fn translate(x: f32, y: f32) -> [f32; 9] {
+    let mut output = [0.0; 9];
+    unsafe {
+        wlr_matrix_translate(output.as_mut_ptr(), x, y);
+    }
+    output
+}
+
+/// Scales the 2D matrix to a magnitude of (x, y).
+pub fn scale(x: f32, y: f32) -> [f32; 9] {
+    let mut output = [0.0; 9];
+    unsafe {
+        wlr_matrix_scale(output.as_mut_ptr(), x, y);
+    }
+    output
+}
+
+/// Rotate the matrix by some amount of radians.
+pub fn rotate(mut matrix: [f32; 9], radians: f32) -> [f32; 9] {
+    unsafe {
+        wlr_matrix_rotate(matrix.as_mut_ptr(), radians);
+    }
+    matrix
+}
+
+/// Multiply two matrices together.
+pub fn multiply(x: [f32; 9], y: [f32; 9]) -> [f32; 9] {
+    let mut output = [0.0; 9];
+    unsafe {
+        wlr_matrix_multiply(output.as_mut_ptr(), x.as_ptr(), y.as_ptr());
+    }
+    output
+}
+
+/// Transform the matrix based on the given Wayland output transform mode.
+pub fn transform(mut matrix: [f32; 9], transform: wl_output_transform) -> [f32; 9] {
+    unsafe {
+        wlr_matrix_transform(matrix.as_mut_ptr(), transform);
+    }
+    matrix
+}
+
+/// Create a 2D orthographic projection matrix of (width, height) with a
+/// specified `wl_output_transform`
+pub fn projection(mut matrix: [f32; 9],
+                         width: i32,
+                         height: i32,
+                         transform: wl_output_transform)
+                         -> [f32; 9] {
+    unsafe {
+        wlr_matrix_projection(matrix.as_mut_ptr(), width, height, transform);
+    }
+    matrix
+}
+
+/// Flip the values over the diagonal of a matrix
+pub fn transpose(matrix: [f32; 9]) -> [f32; 9] {
+    let mut result = [0.0; 9];
+    unsafe {
+        wlr_matrix_transpose(result.as_mut_ptr(), matrix.as_ptr());
+    }
+    result
 }
