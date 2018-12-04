b0bc36cc6e6c139021eded966d929ed6bee2b5e9
Timidger <APragmaticPlace@gmail.com>
2018-12-04 14:55:54 -0500

    Fixed feature flags

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 //! Start your [Compositor](struct.Compositor.html) by implementing an [input
 //! manager](manager/struct.InputManager.html) and an [output
 //! manager](manager/struct.OutputManager.html) on two separate structs.
-#![cfg_attr(not(unstable), allow(unused_imports, unused_macros))]
+#![cfg_attr(not(feature = "unstable"), allow(unused_imports, unused_macros))]
 
 #![allow(unused_unsafe)]
 #[macro_use]
@@ -22,69 +22,68 @@ pub use wlroots_sys::wayland_sys;
 
 #[macro_use]
 mod macros;
-#[cfg(unstable)]
+#[cfg(feature = "unstable")]
 mod manager;
-#[cfg(unstable)]
+#[cfg(feature = "unstable")]
 mod compositor;
-#[cfg(unstable)]
+#[cfg(feature = "unstable")]
 mod errors;
-#[cfg(unstable)]
+#[cfg(feature = "unstable")]
 pub mod events;
-#[cfg(unstable)]
+#[cfg(feature = "unstable")]
 pub mod types;
-#[cfg(unstable)]
+#[cfg(feature = "unstable")]
 pub mod extensions;
-#[cfg(unstable)]
+#[cfg(feature = "unstable")]
 pub mod render;
-#[cfg(unstable)]
+#[cfg(feature = "unstable")]
 pub mod utils;
-#[cfg(unstable)]
+#[cfg(feature = "unstable")]
 mod xwayland;
-#[cfg(unstable)]
+#[cfg(feature = "unstable")]
 mod backend;
 
-#[cfg(unstable)]
-mod unstable {
-    pub use self::backend::*;
-    pub use self::compositor::{compositor_handle, terminate, Compositor, CompositorBuilder,
-                               CompositorHandle, CompositorHandler};
-    pub use self::events::{key_events, seat_events, tablet_pad_events, tablet_tool_events,
-                           touch_events, xwayland_events,
-                           pointer_events::{self, BTN_BACK, BTN_EXTRA, BTN_FORWARD, BTN_LEFT,
-                                            BTN_MIDDLE, BTN_MOUSE, BTN_RIGHT, BTN_SIDE, BTN_TASK},
-                           xdg_shell_v6_events, xdg_shell_events};
-    pub use self::manager::{InputManagerHandler, KeyboardHandler, OutputBuilder, OutputBuilderResult,
-                            OutputHandler, OutputManagerHandler, PointerHandler, TabletPadHandler,
-                            TabletToolHandler, TouchHandler, XdgV6ShellHandler,
-                            XdgV6ShellManagerHandler, XdgShellHandler, XdgShellManagerHandler,
-                            DragIconHandler};
+#[cfg(feature = "unstable")]
+pub use self::backend::*;
+#[cfg(feature = "unstable")]
+pub use self::compositor::{compositor_handle, terminate, Compositor, CompositorBuilder,
+                            CompositorHandle, CompositorHandler};
+#[cfg(feature = "unstable")]
+pub use self::events::{key_events, seat_events, tablet_pad_events, tablet_tool_events,
+                        touch_events, xwayland_events,
+                        pointer_events::{self, BTN_BACK, BTN_EXTRA, BTN_FORWARD, BTN_LEFT,
+                                        BTN_MIDDLE, BTN_MOUSE, BTN_RIGHT, BTN_SIDE, BTN_TASK},
+                        xdg_shell_v6_events, xdg_shell_events};
+#[cfg(feature = "unstable")]
+pub use self::manager::{InputManagerHandler, KeyboardHandler, OutputBuilder, OutputBuilderResult,
+                        OutputHandler, OutputManagerHandler, PointerHandler, TabletPadHandler,
+                        TabletToolHandler, TouchHandler, XdgV6ShellHandler,
+                        XdgV6ShellManagerHandler, XdgShellHandler, XdgShellManagerHandler,
+                        DragIconHandler};
 
-    pub use self::types::area::*;
-    pub use self::types::cursor::*;
-    pub use self::types::data_device::*;
-    pub use self::types::input::*;
-    pub use self::types::output::*;
-    pub use self::types::seat::*;
-    pub use self::types::shell::*;
-    pub use self::types::surface::*;
-    pub use self::xwayland::{XWaylandManagerHandler, XWaylandServer, XWaylandSurface,
-                             XWaylandSurfaceHandle, XWaylandSurfaceHandler, XWaylandSurfaceHints,
-                             XWaylandSurfaceSizeHints};
-    pub use key_events::Key;
-    pub use wlroots_sys::{wlr_keyboard_modifiers, wlr_tablet_tool_axes, wl_shm_format::{self, *},
-                          wlr_axis_orientation::{self, *}, wlr_axis_source::{self, *},
-                          wlr_button_state::{self, *}, wlr_input_device_type::{self, *},
-                          wlr_key_state::{self, *}, wlr_keyboard_modifier::{self, *},
-                          wlr_tablet_pad_ring_source::{self, *},
-                          wlr_tablet_pad_strip_source::{self, *},
-                          wlr_tablet_tool_proximity_state::{self, *}};
+#[cfg(feature = "unstable")]
+pub use self::types::{area::*, cursor::*, data_device::*, input::*,
+                      output::*, seat::*, shell::*, surface::*};
+#[cfg(feature = "unstable")]
+pub use self::xwayland::{XWaylandManagerHandler, XWaylandServer, XWaylandSurface,
+                         XWaylandSurfaceHandle, XWaylandSurfaceHandler, XWaylandSurfaceHints,
+                         XWaylandSurfaceSizeHints};
+#[cfg(feature = "unstable")]
+pub use key_events::Key;
+#[cfg(feature = "unstable")]
+pub use wlroots_sys::{wlr_keyboard_modifiers, wlr_tablet_tool_axes, wl_shm_format::{self, *},
+                      wlr_axis_orientation::{self, *}, wlr_axis_source::{self, *},
+                      wlr_button_state::{self, *}, wlr_input_device_type::{self, *},
+                      wlr_key_state::{self, *}, wlr_keyboard_modifier::{self, *},
+                      wlr_tablet_pad_ring_source::{self, *},
+                      wlr_tablet_pad_strip_source::{self, *},
+                      wlr_tablet_tool_proximity_state::{self, *}};
 
-    pub use self::render::{matrix_identity, matrix_multiply, matrix_projection, matrix_rotate,
-                           matrix_scale, matrix_transform, matrix_translate, matrix_transpose,
-                           project_box, GenericRenderer, Image, Renderer, Texture, TextureFormat};
+#[cfg(feature = "unstable")]
+pub use self::render::{matrix_identity, matrix_multiply, matrix_projection, matrix_rotate,
+                       matrix_scale, matrix_transform, matrix_translate, matrix_transpose,
+                       project_box, GenericRenderer, Image, Renderer, Texture, TextureFormat};
 
-    pub use self::errors::*;
-}
-#[cfg(unstable)]
-pub use unstable::*;
+#[cfg(feature = "unstable")]
+pub use self::errors::*;
 
