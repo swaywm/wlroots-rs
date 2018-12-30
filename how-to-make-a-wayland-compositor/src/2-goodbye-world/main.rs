e4414efedbdfe6871a40900fce28822d9bd19a78
Timidger <APragmaticPlace@gmail.com>
2018-12-30 13:57:32 -0500

    Fixed code to use ctrl+shift+esc, not esc

--- how-to-make-a-wayland-compositor/src/2-goodbye-world/main.rs
+++ how-to-make-a-wayland-compositor/src/2-goodbye-world/main.rs
@@ -3,7 +3,8 @@ extern crate wlroots;
 use wlroots::{compositor,
               input::{self, keyboard},
               utils::log::{WLR_DEBUG, init_logging},
-              xkbcommon::xkb::keysyms};
+              xkbcommon::xkb::keysyms,
+              wlr_key_state::WLR_KEY_PRESSED};
 
 fn main() {
     init_logging(WLR_DEBUG, None);
@@ -18,10 +19,14 @@ fn main() {
 fn keyboard_added(_compositor_handle: compositor::Handle,
                   _keyboard_handle: keyboard::Handle)
                   -> Option<Box<keyboard::Handler>> {
-    Some(Box::new(KeyboardHandler))
+    Some(Box::new(KeyboardHandler::default()))
 }
 
-struct KeyboardHandler;
+#[derive(Default)]
+struct KeyboardHandler {
+    shift_pressed: bool,
+    ctrl_pressed: bool
+}
 
 impl keyboard::Handler for KeyboardHandler {
     fn on_key(&mut self,
@@ -30,7 +35,15 @@ impl keyboard::Handler for KeyboardHandler {
               key_event: &keyboard::event::Key) {
         for key in key_event.pressed_keys() {
             match key {
-                keysyms::KEY_Escape => wlroots::compositor::terminate(),
+                keysyms::KEY_Control_L | keysyms::KEY_Control_R =>
+                    self.ctrl_pressed = key_event.key_state() == WLR_KEY_PRESSED,
+                keysyms::KEY_Shift_L | keysyms::KEY_Shift_R =>
+                    self.shift_pressed = key_event.key_state() == WLR_KEY_PRESSED,
+                keysyms::KEY_Escape => {
+                    if self.shift_pressed && self.ctrl_pressed {
+                        wlroots::compositor::terminate()
+                    }
+                },
                 keysyms::KEY_XF86Switch_VT_1 ..= keysyms::KEY_XF86Switch_VT_12 => {
                     compositor_handle.run(|compositor| {
                         let backend = compositor.backend_mut();
