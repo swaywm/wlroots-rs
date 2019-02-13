103d4f8c030b3fdb082ed151765481861753a043
Seth Barberee <seth.barberee@gmail.com>
2019-02-13 13:29:46 -0500

    use the safe version of seat

--- src/extensions/idle.rs
+++ src/extensions/idle.rs
@@ -1,7 +1,10 @@
 //! Support for the KDE Idle Protocol
 
+
+use seat::Seat;
+
 use wayland_sys::server::wl_display as wl_server_display;
-use wlroots_sys::{wl_display, wlr_seat, wlr_idle, wlr_idle_create,
+use wlroots_sys::{wl_display, wlr_idle, wlr_idle_create,
                   wlr_idle_destroy, wlr_idle_notify_activity,
                   wlr_idle_set_enabled};
 
@@ -22,15 +25,13 @@ impl Manager {
     }
 
     /// Restart the timers for the seat
-    pub fn notify_activity(&mut self, seat: *mut wlr_seat) {
-        unsafe { wlr_idle_notify_activity(self.manager, seat as *mut wlr_seat) }
+    pub fn notify_activity(&mut self, seat: &Seat) {
+        unsafe { wlr_idle_notify_activity(self.manager, seat.as_ptr()) }
     }
 
-    /// If we are passed a null pointer,
-    /// update timers for all seats.
-    pub fn set_enabled(&mut self, seat: *mut wlr_seat, enabled: bool) {
-        wlr_log!(WLR_INFO, "Idle timer status: {:?}", enabled);
-        unsafe { wlr_idle_set_enabled(self.manager, seat as *mut wlr_seat, enabled) }
+    /// If we are passed a null pointer, update timers for all seats.
+    pub fn set_enabled(&mut self, seat: &Seat, enabled: bool) {
+        unsafe { wlr_idle_set_enabled(self.manager, seat.as_ptr(), enabled) }
     }
 }
 
