13ede07c76d55ffe042f96676af57d2edd5c8515
Seth Barberee <seth.barberee@gmail.com>
2019-02-18 17:34:17 -0500

    link to wlr_gamma instead of orbital gamma

--- src/extensions/gamma_control.rs
+++ src/extensions/gamma_control.rs
@@ -1,18 +1,18 @@
 //! Support for the wlroots Gamma Control Protocol
 
 use wayland_sys::server::wl_display as wl_server_display;
-use wlroots_sys::{wl_display, wlr_gamma_control_manager, wlr_gamma_control_manager_create, 
-    wlr_gamma_control_manager_destroy};
+use wlroots_sys::{wl_display, wlr_gamma_control_manager_v1, wlr_gamma_control_manager_v1_create, 
+    wlr_gamma_control_manager_v1_destroy};
 
 #[derive(Debug)]
 /// Manager that can adjust gamma controls for an output
 pub struct Manager {
-    manager: *mut wlr_gamma_control_manager
+    manager: *mut wlr_gamma_control_manager_v1
 }
 
 impl Manager {
     pub(crate) unsafe fn new(display: *mut wl_server_display) -> Option<Self> {
-        let manager_raw = wlr_gamma_control_manager_create(display as *mut wl_display);
+        let manager_raw = wlr_gamma_control_manager_v1_create(display as *mut wl_display);
 
         if !manager_raw.is_null() {
             Some(Manager { manager: manager_raw })
@@ -25,6 +25,6 @@ impl Manager {
 
 impl Drop for Manager {
     fn drop(&mut self) {
-        unsafe { wlr_gamma_control_manager_destroy(self.manager) }
+        unsafe { wlr_gamma_control_manager_v1_destroy(self.manager) }
     }
 }
