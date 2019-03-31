6fc594d08b7c53f1697f24afb54c3ddb44d00e39
Seth Barberee <seth.barberee@gmail.com>
2019-03-31 16:30:52 -0400

    initial attempt at compositor reference

--- src/types/dmabuf.rs
+++ src/types/dmabuf.rs
@@ -1,8 +1,10 @@
 //! Support for the DMABuf type
 
-use wayland_sys::server::wl_display as wl_server_display;
-use wlroots_sys::{wl_display, wlr_renderer, wlr_linux_dmabuf_v1, 
-    wlr_linux_dmabuf_v1_create, wlr_linux_dmabuf_v1_destroy};
+use wlroots_sys::{wl_display, wlr_linux_dmabuf_v1, wlr_linux_dmabuf_v1_create, 
+                wlr_linux_dmabuf_v1_destroy};
+use {compositor::Compositor,
+    render::GenericRenderer};
+
 
 #[derive(Debug)]
 pub struct Dmabuf {
@@ -10,14 +12,28 @@ pub struct Dmabuf {
 }
 
 impl Dmabuf {
-    pub(crate) unsafe fn new(display: *mut wl_server_display, renderer: *mut wlr_renderer) -> Option<Self> {
-        let dmabuf_raw = wlr_linux_dmabuf_v1_create(display as *mut wl_display, renderer);
-        if !dmabuf_raw.is_null() {
-            Some(Dmabuf { dmabuf: dmabuf_raw })
-        } else {
-            None
+    pub fn new(compositor: &Compositor) -> Option<Self> {
+        unsafe {
+            // Get the renderer from compositor
+            let renderer: Option<GenericRenderer> = compositor.renderer;
+            // Unwrap it and check if it's null
+            let renderer_raw = renderer.unwrap().as_ptr();
+            if !renderer_raw.is_null() {
+                // We found a renderer
+                let dmabuf_raw = wlr_linux_dmabuf_v1_create(compositor.display as *mut wl_display, renderer_raw);
+                if !dmabuf_raw.is_null() {
+                    Some(Dmabuf { dmabuf: dmabuf_raw })
+                } else {
+                    None
+                }
+            }
+            else {
+                 // No renderer found so return none
+                None
+            }
         }
     }
+    // TODO wrap rest of dmabuf
 }
 
 impl Drop for Dmabuf {
