4f5a80a8e8fe140940a98afdf40ea802365a23ad
Seth Barberee <seth.barberee@gmail.com>
2019-04-02 22:22:07 -0500

    address review comments

--- src/types/dmabuf.rs
+++ src/types/dmabuf.rs
@@ -2,8 +2,8 @@
 
 use wlroots_sys::{wl_display, wlr_linux_dmabuf_v1, wlr_linux_dmabuf_v1_create, 
                 wlr_linux_dmabuf_v1_destroy};
-use {compositor::Compositor,
-    render::GenericRenderer};
+use {crate::compositor::Compositor,
+    crate::render::GenericRenderer};
 
 
 #[derive(Debug)]
@@ -15,20 +15,11 @@ impl Dmabuf {
     pub fn new(compositor: &Compositor) -> Option<Self> {
         unsafe {
             // Get the renderer from compositor
-            let renderer: Option<GenericRenderer> = compositor.renderer;
-            // Unwrap it and check if it's null
-            let renderer_raw = renderer.unwrap().as_ptr();
-            if !renderer_raw.is_null() {
-                // We found a renderer
-                let dmabuf_raw = wlr_linux_dmabuf_v1_create(compositor.display as *mut wl_display, renderer_raw);
-                if !dmabuf_raw.is_null() {
-                    Some(Dmabuf { dmabuf: dmabuf_raw })
-                } else {
-                    None
-                }
-            }
-            else {
-                 // No renderer found so return none
+            let renderer: Option<&GenericRenderer> = compositor.renderer.as_ref();
+            let dmabuf_raw = wlr_linux_dmabuf_v1_create(compositor.display as *mut wl_display, renderer.unwrap().as_ptr());
+            if !dmabuf_raw.is_null() {
+                Some(Dmabuf { dmabuf: dmabuf_raw })
+            } else {
                 None
             }
         }
